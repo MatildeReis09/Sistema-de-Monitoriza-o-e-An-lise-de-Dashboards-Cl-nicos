a4aa0bd1e506df075778d97f1490cb192edddddc
Matilde Reis <a28048@alunos.ipca.pt>
2026-01-31 19:10:47 +0000

    escolha dataset + ligação api kaggle

---
+++ data.py
@@ -0,0 +1,7 @@
+import kagglehub
+
+# Download latest version
+##importar dataset retirado do kaggle
+path = kagglehub.dataset_download("uom190346a/synthetic-clinical-tabular-dataset")
+
+print("Path to dataset files:", path)
