655419b108be524e9b8c3bbbcb29a860791e0119
Matilde Reis <a28048@alunos.ipca.pt>
2026-01-31 19:27:15 +0000

    atualização de infromação

---
+++ Base_dados.sql
@@ -0,0 +1,35 @@
+
+CREATE TABLE Person (
+id serial primary key,
+name varchar (200) NOT NULL, 
+date_of_birth Date, 
+sex varchar
+);
+
+
+CREATE TABLE Consult(
+id serial primary key,
+pacient_id int, 
+consult_date DATE,
+CONSTRAINT FK_pacient_id FOREIGN KEY (pacient_id) REFERENCES Person (id)
+);
+
+CREATE TABLE Read_data(
+id serial primary key,
+pacient_id int,
+consult_id int,
+read_type varchar(200),
+unid varchar(20),
+value decimal(10,2),
+date_time TIMESTAMP default current_timestamp,
+CONSTRAINT FK_pacient_id FOREIGN KEY (pacient_id) REFERENCES Person (id),
+CONSTRAINT FK_consult_id FOREIGN KEY (consult_id) REFERENCES Consult (id)
+);
+
+CREATE TABLE Alert(
+id serial primary key,
+read_id int,
+message TEXT,
+CONSTRAINT FK_read_id FOREIGN KEY (read_id) REFERENCES Read_data (id)
+);
+
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Sistema-de-Monitoriza-o-e-An-lise-de-Dashboards-Cl-nicos
+
+Tecnologias Utilizadas:
+
+Base de Dados - PostgreSQL ( Modelação e Triggers)
+Linguagem - Python ( Pandas , ETL e análise preditiva)
+BI - Power BI/Excel ( Dashboards de apoio à decisão)
+
+
+
+1- Requisitos 
+
+-Python
+-PostgreSQL
+-Power BI 
+
+2- Base de Dados (SQL)
+
+
+3- Configuração do Ambiente Python 
+
+Dentro da pasta do projeto, no ambiente virtual abrir o terminal e instalar as bibliotecas necessárias
+
+'pip install kagglehub pandas psycopg2 sqlalchemy'
+
+4- Processo ETL
+
+
+5- Visualização de Dados 
+
+Abrir o Power BI, conectar-se á base de dados  PostgreSQL (ClinicaData) e carregar as tabelas para criar as Dashboard.
